2580fe8cd396040d741beeba78890fd8678fae0d
Guilherme Murata <123330836+GuilhermeMurata@users.noreply.github.com>
2024-08-08 19:04:39 -0300

    Add encapsulamento salario

--- funcionario.js
+++ funcionario.js
@@ -1,7 +1,35 @@
 function Pessoa(nome) {
     this.nome = nome;
+    this.dizOi = function() {
+        console.log(this.nome + " diz ola");
+    }
+    this.dizCargo = function() {
+        console.log(this.cargo);
+    }
 }
 
-const pessoa1 = new Pessoa("Maria");
+function Funcionario(nome, cargo, salario) {
+    this.cargo = cargo;
+    // this.salario = salario;
+    let _salario = salario;
 
-console.log(pessoa1);
+    this.getSalario = function() {
+        return `O salario de ${this.nome} e ${_salario}`;
+    }
+
+    this.setSalario = function(valor) {
+        if (typeof valor === 'number') {
+            _salario = valor;
+        }
+    }
+
+    Pessoa.call(this, nome);
+}
+
+const funcionario1 = new Funcionario("Maria", "Dev front-end", 5000);
+funcionario1.dizOi();
+funcionario1.dizCargo();
+
+funcionario1.setSalario("mil");
+
+console.log(funcionario1.getSalario());
